8044670761ee0167bd574d7b590b4ecb8b9caec8
Arkanzier <davidd.albert@gmail.com>
2023-04-17 22:26:57 -0700

    Minor upgrades to Spell class.

--- Character/Spell.cs
+++ Character/Spell.cs
@@ -6,46 +6,46 @@
         //Basic information
         //-------- -------- -------- -------- -------- -------- -------- -------- 
         //An identifier for behind-the-scenes use.
-        public string id { get; private set; }
+        public string ID { get; set; }
         //The name to display to the user.
-        public string name { get; private set; }
+        public string Name { get; set; }
         //The spell's level, 0 for cantrip.
-        public int level { get; private set; }
+        public int Level { get;  set; }
 
         //Components
         //-------- -------- -------- -------- -------- -------- -------- -------- 
         //Whether the spell has a vocal component.
-        public bool vocal { get; private set; }
+        public bool Vocal { get; set; }
         //Whether the spell has a somatic component.
-        public bool somatic { get; private set; }
-        //Whether the spell has a material component.
-        public bool material { get; private set; }
-        //to do: consider switching this to something other than a bool.
-        //or use this for no-price material components only?
+        public bool Somatic { get; set; }
+        //Whether the spell has a material component with no cost listed.
+        public bool Material { get; set; }
+        //Whether the spell has an expensive material component.
+        public string ExpensiveMaterial { get; set; }
 
         //Basic Information
         //-------- -------- -------- -------- -------- -------- -------- -------- 
         //Which type of action the spell takes.
-        public string action { get; private set; }
+        public string Action { get; set; }
         //Proper description
-        public string description { get; private set; }
+        public string Description { get; set; }
         //What benefit, if any, the spell gets for being cast at a higher level.
         //Empty string for none.
-        public string upcastingBenefit { get; private set; }
+        public string UpcastingBenefit { get; set; }
         //The spell's range.
-        public string range { get; private set; }
+        public string Range { get; set; }
         //Duration in rounds.
-        public int duration { get; private set; }
+        public int Duration { get; set; }
         //or do i want to make this a string and just let the player deal with it?
         //Whether the spell requires concentration.
-        public bool concentration { get; private set; }
+        public bool Concentration { get; set; }
 
         //Meta information
         //-------- -------- -------- -------- -------- -------- -------- -------- 
         //Which book the spell is from.
-        public string book { get; private set; }
+        public string Book { get; set; }
         //Which page number of the book, if appropriate. 0 for none.
-        public int page { get; private set; }
+        public int Page { get; set; }
 
         //to add:
         //something to store dice collections + descriptors for them.
@@ -56,46 +56,58 @@
         //-------- -------- -------- -------- -------- -------- -------- -------- 
         public Spell()
         {
-            id = "";
-            name = "";
-            level = 0;
-            vocal = false;
-            somatic = false;
-            material = false;
-            action = "";
-            description = "";
-            upcastingBenefit = "";
-            range = "";
-            duration = 0;
-            concentration = false;
-            book = "";
-            page = 0;
+            ID = "";
+            Name = "";
+            Level = 0;
+            Vocal = false;
+            Somatic = false;
+            Material = false;
+            ExpensiveMaterial = "";
+            Action = "";
+            Description = "";
+            UpcastingBenefit = "";
+            Range = "";
+            Duration = 0;
+            Concentration = false;
+            Book = "";
+            Page = 0;
+        }
+        public Spell(string id)
+            : this()
+        {
+            ID = id;
+            //look up the spell by ID within a db of spells, then load it's info.
         }
 
         public Spell(string id, string name, int level, bool vocal, bool somatic, bool material, string action, string description, string upcastingBenefit, string range, int duration, bool concentration = false, string book = "", int page = 0)
+            : this()
         {
             //to do: decide how many of these i want to make optional
-            this.id = id;
-            this.name = name;
-            this.level = level;
-            this.vocal = vocal;
-            this.somatic = somatic;
-            this.material = material;
-            this.action = action;
-            this.description = description;
-            this.upcastingBenefit = upcastingBenefit;
-            this.range = range;
-            this.duration = duration;
-            this.concentration = concentration;
-            this.book = book;
-            this.page = page;
+            this.ID = id;
+            this.Name = name;
+            this.Level = level;
+            this.Vocal = vocal;
+            this.Somatic = somatic;
+            this.Material = material;
+            //ExpensiveMaterial
+            this.Action = action;
+            this.Description = description;
+            this.UpcastingBenefit = upcastingBenefit;
+            this.Range = range;
+            this.Duration = duration;
+            this.Concentration = concentration;
+            this.Book = book;
+            this.Page = page;
         }
 
         public Spell (string id, string name)
             : this()
         {
-            this.id = id;
-            this.name= name;
+            this.ID = id;
+            this.Name= name;
         }
+
+        //
+        //-------- -------- -------- -------- -------- -------- -------- -------- 
     }
 }
